f95f16656d8b57e66b54c163596017aab79d820a
Clarkie <andy.clarke@wonderbill.com>
2020-03-25 20:32:47 +0000

    add env vars

--- handler.js
+++ handler.js
@@ -1,18 +1,15 @@
-'use strict';
+"use strict";
+
+const { SLACK_WEBHOOK, CHANNEL_NAME } = process.env;
 
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return sendMessage("testing");
 };
+
+const sendMessage = message =>
+  axios.post(SLACK_WEBHOOK, {
+    channel: CHANNEL_NAME,
+    username: "WonderWorkout",
+    text: message,
+    icon_emoji: ":weight_lifter:"
+  });
